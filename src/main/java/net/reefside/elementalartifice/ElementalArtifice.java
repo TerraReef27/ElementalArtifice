eceae400f116120bc62ece655b9e7973569b5476
TerraReef27 <terrareef27@gmail.com>
2022-02-08 17:59:26 -0500

    Initial Commit

---
+++ src/main/java/net/reefside/elementalartifice/ElementalArtifice.java
@@ -0,0 +1,13 @@
+package net.reefside.elementalartifice;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("elementalartifice")
+public class ElementalArtifice {
+
+    public static final String MODID = "elementalartifice";
+
+    // Register ourselves for server and other game events we are interested in
+    //MinecraftForge.EVENT_BUS.register(this);
+}
