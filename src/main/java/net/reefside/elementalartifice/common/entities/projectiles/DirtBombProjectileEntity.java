10139f79c30c7e671f79313a43e1dfac0c373cbc
TerraReef27 <terrareef27@gmail.com>
2022-02-16 22:08:00 -0500

    Dirtbomb has proper build pattern

--- src/main/java/net/reefside/elementalartifice/common/entities/projectiles/DirtBombProjectileEntity.java
+++ src/main/java/net/reefside/elementalartifice/common/entities/projectiles/DirtBombProjectileEntity.java
@@ -1,5 +1,6 @@
 package net.reefside.elementalartifice.common.entities.projectiles;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ItemParticleOption;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
@@ -12,6 +13,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
@@ -46,8 +49,6 @@ public class DirtBombProjectileEntity extends ThrowableItemProjectile {
             for(int i=0; i<100; i++) {
                 this.level.addParticle(particleOptions, this.getX(), this.getY(), this.getZ(), 10.0D,  10.0D, 10.0D);
             }
-
-
         }
     }
 
@@ -55,7 +56,7 @@ public class DirtBombProjectileEntity extends ThrowableItemProjectile {
         System.out.println("Hit entity");
         super.onHitEntity(entityHitResult);
         Entity entity = entityHitResult.getEntity();
-        entity.hurt(DamageSource.thrown(this, this.getOwner()), 10F);
+        entity.hurt(DamageSource.thrown(this, this.getOwner()), 100F);
     }
 
     protected void onHit(HitResult hitResult) {
@@ -63,8 +64,8 @@ public class DirtBombProjectileEntity extends ThrowableItemProjectile {
         if(!this.level.isClientSide) {
             this.level.broadcastEntityEvent(this, (byte)3);
             System.out.println("Hit Position: " + this.blockPosition());
-            //this.level.gameEvent(this.getOwner(), GameEvent.BLOCK_PLACE, this.blockPosition());
-            level.setBlockAndUpdate(this.blockPosition(), ArtBlocks.TEST_BLOCK.get().defaultBlockState());
+            BlockState block = Blocks.DIRT.defaultBlockState();
+            createCluster(this.blockPosition(), block, 3);
             this.discard();
         }
     }
@@ -72,5 +73,19 @@ public class DirtBombProjectileEntity extends ThrowableItemProjectile {
     private ParticleOptions getParticle() {
         ItemStack itemstack = this.getItemRaw();
         return (ParticleOptions)(itemstack.isEmpty() ? ParticleTypes.ASH : new ItemParticleOption(ParticleTypes.ITEM, itemstack));
+    }
+
+    private void createCluster(BlockPos blockPos, BlockState block, int size) {
+            int circumference = size*size+1;
+            for(int i= -circumference; i<circumference; i++) {
+                for(int j= -circumference; j<circumference; j++) {
+                    for(int k= -circumference; k<circumference; k++) {
+                        int d = i*i + j*j + k*k;
+                        if(d < circumference-1) {
+                            level.setBlockAndUpdate(blockPos.offset(i, j, k), block);
+                        }
+                    }
+                }
+            }
     }
 }
